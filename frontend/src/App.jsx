797ed8256a78efbf5dc56d45ea308d868005ed67
Lavanya Dhiman <iamlavanya2004@gmail.com>
2025-07-01 19:05:25 +0530

    Done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import "prismjs/themes/prism-tomorrow.css";
+import Editor from "react-simple-code-editor";
+import prism from "prismjs";
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [code, setCode] = useState(`function sum() {
+  return 1 + 1;
+}`);
+
+  const [review, setReview] = useState(``);
+
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  async function reviewCode() {
+    try {
+      const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+      setReview(response.data);
+    } catch (error) {
+      setReview("Error fetching review. Please try again.");
+    }
+  }
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={12}
+              style={{
+                fontFamily: '"Fira Code", "Fira Mono", monospace',
+                fontSize: 16,
+                borderRadius: "0.7rem",
+                height: "100%",
+                width: "100%",
+                color: "#f8f8f2",
+                backgroundColor: "#0c0c0c"
+              }}
+            />
+          </div>
+          <div onClick={reviewCode} className="review">
+             Review
+          </div>
+        </div>
+        <div className="right">
+          <Markdown rehypePlugins={[rehypeHighlight]}>
+            {review}
+          </Markdown>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
