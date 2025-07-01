190dbd9c0ad02bc2ba0549acd98a50166aa96599
Lavanya Dhiman <iamlavanya2004@gmail.com>
2025-07-01 19:24:02 +0530

    .env setup to deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,10 @@ function App() {
 
   async function reviewCode() {
     try {
-      const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/ai/get-review`,
+        { code }
+      );
       setReview(response.data);
     } catch (error) {
       setReview("Error fetching review. Please try again.");
@@ -50,7 +53,7 @@ function App() {
             />
           </div>
           <div onClick={reviewCode} className="review">
-             Review
+            Review
           </div>
         </div>
         <div className="right">
